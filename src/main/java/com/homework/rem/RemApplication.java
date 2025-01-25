776fbd8ceaaf93536d4257afba93beaf18a6b608
Filip Działa <fi.dziala@gmail.com>
2025-01-25 07:19:47 +0100

    init commit. Added db-template.properties configured liquibase, added changelog file.

--- src/main/java/com/homework/rem/RemApplication.java
+++ src/main/java/com/homework/rem/RemApplication.java
@@ -2,7 +2,9 @@ package com.homework.rem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class RemApplication {
 
